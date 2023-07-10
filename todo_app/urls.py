85c2f433a88bbf6a0cf74fca4bbdcda3a8f4f71b
Darika34 <darikaakmatsaeva@gmail.com>
2023-07-10 16:37:09 +0600

    first commit

---
+++ todo_app/urls.py
@@ -0,0 +1,11 @@
+from django.urls import include, path
+from rest_framework import routers
+from .views import CategoryViewSet, TodoViewSet
+
+
+router = routers.DefaultRouter()
+router.register(r'categories', CategoryViewSet)
+router.register(r'todos', TodoViewSet)
+urlpatterns = [
+    path('', include(router.urls)),
+]
---
+++ todo_app/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Category, Todo
+from .serializers import CategorySerializer, TodoSerializer
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+class TodoViewSet(viewsets.ModelViewSet):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
+# Create your views here.
